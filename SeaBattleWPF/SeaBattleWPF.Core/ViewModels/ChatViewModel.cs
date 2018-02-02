12631567c83147cf5b144f54b55e0f64c58c7fb0
SaNdBoXa <dima_nikishin_84@mail.ru>
2018-02-02 19:17:08 +0300

    alpha version 1.0

--- SeaBattleWPF/SeaBattleWPF.Core/ViewModels/ChatViewModel.cs
+++ SeaBattleWPF/SeaBattleWPF.Core/ViewModels/ChatViewModel.cs
@@ -9,20 +9,7 @@ namespace SeaBattleWPF.Core.ViewModels
 
         public ChatViewModel(IServerHandlerService serverHandlerService)
         {
-            serverHandlerService.Message += ServerHandlerService_Message;
-            serverHandlerService.Shoot += ServerHandlerService_Shoot;
-        }
 
-        private void ServerHandlerService_Shoot(Message message)
-        {
-            Message = message.message;
-            OnPropertyChanged(nameof(Message));
-        }
-
-        private void ServerHandlerService_Message(Message message)
-        {
-            Message = message.message;
-            OnPropertyChanged(nameof(Message));
-        }
+        }      
     }
 }
